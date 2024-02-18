bbce007db37104fea4870f5a5f824f5ba90e8e15
qiwihui <qwh005007@gmail.com>
2024-02-18 02:55:30 +0000

    Deploying to gh-pages from @ qiwihui/MyWalletScan@06cdbc6dca77bd5d24947f8e4add00bc899d92ca 🚀

--- assets/deleteData-48c82ec5.js
+++
@@ -1,1 +0,0 @@
-import{i as e,d as c,v as n,N as l}from"./index-8ea3f705.js";const m=async(o,i)=>{try{await e(c);const a=i.map(async s=>{const t=await n(o,s);console.log(t),t&&await l(o,s)});await Promise.all(a)}catch(a){console.log(a)}};export{m as d};
